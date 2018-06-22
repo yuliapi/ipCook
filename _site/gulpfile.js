e240b3c42ab8688a2778ab850c7fd0aefaeaab63
yulia <zEEcNCTDud0l>
2018-06-22 12:36:21 -0700

    Switch to gulp for building jekyll project.

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -1,8 +1,28 @@
-var gulp = require('gulp');
+
+
+// Define variables.
+let autoprefixer = require('autoprefixer');
 var $ = require('gulp-load-plugins')();
+var browserSync  = require('browser-sync').create();
+var cleancss     = require('gulp-clean-css');
+var concat       = require('gulp-concat');
+var del          = require('del');
+var gulp         = require('gulp');
+var gutil        = require('gulp-util');
+var imagemin     = require('gulp-imagemin');
+var notify       = require('gulp-notify');
+var postcss      = require('gulp-postcss');
+var rename       = require('gulp-rename');
+var run          = require('gulp-run');
+var runSequence  = require('run-sequence');
+var sass         = require('gulp-ruby-sass');
+var uglify       = require('gulp-uglify');
+
+// Include paths file.
+var paths = require('./_assets/gulp_config/paths');
 
 gulp.task('images', function () {
-    return gulp.src('images/*.{jpg,png}')
+    return gulp.src('_assets/images/*.{jpg,png}')
         .pipe($.responsive({
             '*': {
                 // Resize all JPG images to 200 pixels wide
